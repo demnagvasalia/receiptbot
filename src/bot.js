8b8b410b8c56a9008bba98c69b463b0922bf65d2
zeydlol <aeron.porter@gmail.com>
2024-02-08 15:25:38 -0800

    added multi instance

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const readline = require('readline');
 const { CommandHandler } = require('djs-commander');
 const { Client, GatewayIntentBits} = require('discord.js')
 const path = require('path');
@@ -10,18 +11,40 @@ new CommandHandler({
     commandsPath: path.join(__dirname, 'commands')
 });
 
-(async () => {
-    try {
-        mongoose.set('strictQuery', false)
-        await mongoose.connect(process.env.MONGODB_URI, { });
-            console.log("Connected to database");
-    } catch (error) {
-        console.log(`Error: ${error}`);
-    }
-})();
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-client.login(process.env.BOT_TEST);
+rl.question('Enter instance number (1 or 2): ', (instanceNumber) => {
+    rl.close();
+    let mongodb_uri = ""
+    let bot_token = ""
+    switch(instanceNumber) {
+        case "1": {
+            mongodb_uri = process.env.DB_FIRST
+            bot_token = process.env.BOT_FIRST
+            break;
+        }
+        case "2": {
+            mongodb_uri = process.env.DB_SECOND
+            bot_token = process.env.BOT_SECOND
+            break;
+        }
+    }
+    (async () => {
+        try {
+            mongoose.set('strictQuery', false)
+            await mongoose.connect(mongodb_uri, {});
+            console.log("Connected to database");
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    })();
+    console.log(bot_token);
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+    });
+
+    client.login(bot_token);
+});
