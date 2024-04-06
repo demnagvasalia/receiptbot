e90050a6589b7889e5ce5ac3ecaae2cf999784fc
zeydlol <aeron.porter@gmail.com>
2024-04-07 00:20:19 +0200

    dont commit bot.js anymore!

--- src/bot.js
+++ src/bot.js
@@ -15,40 +15,19 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-rl.question('Enter instance number (1 or 2): ', (instanceNumber) => {
-    rl.close();
-    let mongodb_uri = ""
-    let bot_token = ""
-    switch(instanceNumber) {
-        case "test": {
-            mongodb_uri = process.env.DB_TEST
-            bot_token = process.env.BOT_TEST
-            break;
-        }
-        case "1": {
-            mongodb_uri = process.env.DB_FIRST
-            bot_token = process.env.BOT_FIRST
-            break;
-        }
-        case "2": {
-            mongodb_uri = process.env.DB_SECOND
-            bot_token = process.env.BOT_SECOND
-            break;
-        }
+mongodb_uri = process.env.DB_FIRST
+bot_token = process.env.BOT_FIRST
+(async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(mongodb_uri, {});
+    } catch (error) {
+        console.log(`Error: ${error}`);
     }
-    (async () => {
-        try {
-            mongoose.set('strictQuery', false)
-            await mongoose.connect(mongodb_uri, {});
-        } catch (error) {
-            console.log(`Error: ${error}`);
-        }
-    })();
-    console.log(bot_token);
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-    });
-
-    client.login(bot_token);
+})();
+console.log(bot_token);
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.login(bot_token);
